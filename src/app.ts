c517d6d971f96c97bb61d7ebeccd1b46fa51759b
Chrismar Ilustrisimo <chrismarilustrisimo9@gmail.com>
2025-10-23 18:40:21 +0800

    feat: update app.ts for running server

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,5 @@ app.get("/", (_, res) => {
   res.send("✅ WebTools API is running!");
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
-});
+
+export default app;
