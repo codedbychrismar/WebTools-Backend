e35e5fb00a0ae4ed1c8d6064cfc5e180f3dfab16
Chrismar Ilustrisimo <chrismarilustrisimo9@gmail.com>
2025-10-23 22:50:56 +0800

    feat: update cors policy

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,14 @@
 import { createServer } from "http";
 import app from "./app";
 import express from "express";
+import cors from "cors";
 
 app.use(express.json());
+
+app.use(cors({
+  origin: ["http://localhost:5173"],
+  methods: ["GET", "POST", "PATCH", "DELETE"],
+}));
 
 
 const PORT = process.env.PORT || 5000;
